211a5c25e591f3eb36ad87bcde5866d9b278eba2
amitkrdev <amitxprogramme@gmail.com>
2025-05-16 17:17:07 +0530

    Set up splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Splash Api
+    implementation(libs.androidx.core.splashscreen)
+
 }
